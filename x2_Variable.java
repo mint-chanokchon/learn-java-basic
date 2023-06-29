18c41ffe5d4c0350c6027c222d8827de9dea206f
Colorfuls <bike.chanokchon@gmail.com>
2023-06-29 16:01:58 +0700

    variable

---
+++ x2_Variable.java
@@ -0,0 +1,18 @@
+public class x2_Variable {
+    public static void main(String[] args) {
+        int x; // declaration
+        x = 123; // assignment
+        int y = 123; // initialization
+
+        float a = 3.14f;
+        double b = 3.14;
+        int c = 10;
+        char symbol = '@';
+        String d = "Hello World";
+
+        System.out.println(x);
+        System.out.println(y);
+
+        // https://youtu.be/xk4_1vDrzzo?t=2025
+    }
+}
